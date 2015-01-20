282be4fe4ea1dc6ef4e966714925e5a5a3aa2c46
Nolan Brubaker <nolan.brubaker@rackspace.com>
2015-01-20 15:30:43 -0500

    Fix typo in find_packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Set of commands that scrape destinytracker.com for info.',
     author='Nolan Brubaker',
     author_email='palendae@gmail.com',
-    packages=find_package(),
+    packages=find_packages(),
     py_modules=['destiny_bot'],
     include_package_data=True,
     zip_safe=True,
