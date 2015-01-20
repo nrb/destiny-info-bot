bc7602a59349686b688c162c7b5d8b8910d58a75
Nolan Brubaker <nolan.brubaker@rackspace.com>
2015-01-20 17:07:18 -0500

    Fix soup call in the helga plugins.

--- destiny_bot.py
+++ destiny_bot.py
@@ -1,15 +1,16 @@
 from helga.plugins import command
 
-from scraper import nightfall_info, heroic_info
+from scraper import nightfall_info, heroic_info, get_soup
 
 nf_help = 'Displays info on the current nightfall from destinytracker.com'
 
 @command('nightfall', aliases=('nf',), help=nf_help)
 def nightfall(client, channel, nick, message, cmd, args):
-    return nightfall_info()
+    soup = get_soup()
+    return nightfall_info(soup)
 
 hero_help = 'Displays info on the current heroic from destinytracker.com'
 @command('heroic', aliases=('hero',), help=hero_help)
 def heroic(client, channel, nick, message, cmd, args):
-    return heroic_info()
-
+    soup = get_soup()
+    return heroic_info(soup)
