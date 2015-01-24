65852a28e1ebea75667c7fc66f202607c6fb2f9f
Nolan Brubaker <nolan@nbrubaker.com>
2015-01-24 17:32:08 -0500

    Bump version for added scraping target.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='destiny_bot',
-    version='0.0.7',
+    version='0.0.8',
     description='Set of commands that scrape destinytracker.com for info.',
     author='Nolan Brubaker',
     author_email='palendae@gmail.com',
