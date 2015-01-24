945457a2ec86819a02bfd89f1d17d145b5d47351
Nolan Brubaker <nolan.brubaker@rackspace.com>
2015-01-23 20:42:00 -0500

    Adds planet destiny scraping for the daily
    This is for issue #1. Will still need to hook it up to the bot, though.

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ def get_mods(soup, index):
 
     return modifiers
 
-def get_soup():
+def get_soup(event_url=event_url):
     response = requests.get(event_url)
     html = response.text
 
@@ -36,7 +36,22 @@ def heroic_info(soup):
     return template % {'strike': strike_title(soup),
                        'mods': heroic_mods(soup)}
 
+
+def daily_info():
+    soup = get_soup("http://db.planetdestiny.com/events")
+
+    title = soup.select('div.activity-title')[0].text
+    title = title.strip()
+
+    mod_list = soup.select('div.tooltip-caption')[0:2]
+    modifiers = [div.text for div in mod_list]
+    modifiers = ', '.join(modifiers)
+
+    return template % {'strike': title, 'mods': modifiers}
+
+
 if __name__ == '__main__':
     soup = get_soup()
     print nightfall_info(soup)
     print heroic_info(soup)
+    print daily_info()
