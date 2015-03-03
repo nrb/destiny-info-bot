edff0943dc47e8892da77637ca61dc25d5affefc
Nolan Brubaker <nolan@nbrubaker.com>
2015-03-02 21:31:24 -0500

    Add checks for bounties and crucible

--- check.py
+++ check.py
@@ -3,16 +3,28 @@
 import sys
 
 from functions import daily_lookup,  heroic_lookup, nightfall_lookup
+from functions import bounty_lookup, crucible_lookup, VENDOR_NAMES
+
+def check_bounties():
+    bounties = []
+    for name in VENDOR_NAMES:
+        bounties.extend([name, '---'])
+        bounties.append(bounty_lookup(name))
+    return '\n'.join(bounties)
+
 
 def main(command):
-    if command not in ('nightfall', 'heroic', 'daily'):
-        return "%s was not a valid command" % command
-
     cmd_map = {
         'daily': daily_lookup,
         'heroic': heroic_lookup,
         'nightfall': nightfall_lookup,
+        'crucible': crucible_lookup,
+        'bounties': check_bounties,
+
     }
+
+    if command not in cmd_map.keys():
+        return "%s was not a valid command" % command
 
     func = cmd_map[command]
     return func()
